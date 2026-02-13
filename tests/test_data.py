597f29a8dda65e7f161bcf26421aa6e65b9f5a65
Johan Schmidt <johan_schmidt@live.dk>
2026-02-13 21:46:58 +0100

    Commiting test changes back to normal

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,8 @@
-# import pytest
+import pytest
 
 
-# def test_code_is_tested():
-#    assert False
+def test_code_is_tested():
+    assert False
 
-from src.danlonben.testing import test
 
-test()
 
